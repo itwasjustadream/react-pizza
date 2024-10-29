f102ccd216ffc7d8436b9fd35016c3d77f580774
itwasjustaream <123476982+itwasjustadream@users.noreply.github.com>
2024-10-29 21:12:51 +0300

    feat: move data to Backend(Mokky.dev) and add fetch in useEffect hook

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizzas.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://405ee8d1da7c9e4f.mokky.dev/items')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -21,7 +27,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
