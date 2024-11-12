5801ce30f88ad056cc7dfb244a340c5c17e6a503
itwasjustaream <123476982+itwasjustadream@users.noreply.github.com>
2024-11-13 01:58:34 +0300

    feat: add sorting and filtering(backend queries)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,31 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 const Home = () => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://405ee8d1da7c9e4f.mokky.dev/items')
+    setIsLoading(true);
+
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(`https://405ee8d1da7c9e4f.mokky.dev/items?${category}&sortBy=${sortType.sortProperty}`)
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
