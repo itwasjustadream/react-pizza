f529758dbdec38ec8ce5b28f89b53866b5dc0b4e
itwasjustaream <123476982+itwasjustadream@users.noreply.github.com>
2025-01-09 01:10:30 +0300

    feat: add pages for every Pizza Card using useParams, add Cart button hiding using useLocation and refactor routes with Outlet

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const FullPizza = () => {
+  const [pizza, setPizza] = React.useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get('https://405ee8d1da7c9e4f.mokky.dev/items/' + id);
+        setPizza(data);
+      } catch (error) {
+        alert('Ошибка при получении пиццы');
+        navigate('/');
+      }
+    }
+
+    fetchPizza();
+  }, []);
+
+  if (!pizza) {
+    return;
+  }
+
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} />
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} ₽</h4>
+    </div>
+  );
+};
+
+export default FullPizza;
